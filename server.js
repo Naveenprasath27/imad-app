6057bf42def5fb83ac790108274d9a6d6eb10c6f
Naveenprasath27 <30733156+Naveenprasath27@users.noreply.github.com>
2017-08-08 21:39:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/about-lunareclipse', function (req, res) {
+app.get('/ui/about-lunareclipse', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui','about-lunareclipse.html'));
 });
 
