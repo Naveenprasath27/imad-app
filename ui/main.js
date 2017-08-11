36a71adc2ffc8c34deb0b24c19b911cc3090a3f9
Naveenprasath27 <30733156+Naveenprasath27@users.noreply.github.com>
2017-08-11 15:38:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element=document.getElementById('contents');
 
 element.innerHTML='Changed element';
 var img=document.getElementById('dino');
+function moveRight(){
+    var marginLeft=0;
+    img.style.marginLeft=marginLeft+'10px';
+}
+
 img.onclick=function() {
 
-img.style.marginLeft='100px';};
+var interval=setInterval(moveRight,100);}
