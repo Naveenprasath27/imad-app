7cf6c764ff5552f573580bc25ce78376d71cb50b
Naveenprasath27 <30733156+Naveenprasath27@users.noreply.github.com>
2017-08-11 15:15:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,9 @@ app.get('/:aboutName', function (req, res) {
   res.send(createtemplate(eclipse[aboutName]));
 });
 
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
