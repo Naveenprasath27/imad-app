98c2fadc0974c2c451560d1be645354c443275b3
Naveenprasath27 <30733156+Naveenprasath27@users.noreply.github.com>
2017-08-11 15:28:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('contents');
 
 element.innerHTML='Changed element';
+var img=document.getElementById('dino');
+img.onclick=function() {
+
+img.style.marginLeft='100px';};
